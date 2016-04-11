bce5fa0d2fb7809dc8c07f371fe92168541dde11
derekscode <derekscode@github.com>
2016-04-11 15:57:03 -0700

    Angular 2 working now

--- src/Angular2AspNetCoreSeed/gulpfile.js
+++ src/Angular2AspNetCoreSeed/gulpfile.js
@@ -1,4 +1,5 @@
-﻿/// <binding Clean='clean' />
+﻿/// <binding AfterBuild='moveToLibs' Clean='clean' />
+
 "use strict";
 
 var gulp = require("gulp"),
@@ -43,3 +44,27 @@ gulp.task("min:css", function () {
 });
 
 gulp.task("min", ["min:js", "min:css"]);
+
+
+
+
+// move libraries to wwwroot
+gulp.task('moveToLibs', function (done) {
+    gulp.src([
+      'node_modules/angular2/bundles/js',
+      'node_modules/angular2/bundles/angular2.*.js*',
+      'node_modules/angular2/bundles/angular2-polyfills.js',
+      'node_modules/angular2/bundles/http.*.js*',
+      'node_modules/angular2/bundles/router.*.js*',
+      'node_modules/es6-shim/es6-shim.min.js*',
+      'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js',
+      'node_modules/systemjs/dist/*.*',
+      'node_modules/jquery/dist/jquery.*js',
+      'node_modules/bootstrap/dist/js/bootstrap*.js',
+      'node_modules/rxjs/bundles/Rx.js'
+    ]).pipe(gulp.dest('./wwwroot/libs/'));
+
+    gulp.src([
+      'node_modules/bootstrap/dist/css/bootstrap.css'
+    ]).pipe(gulp.dest('./wwwroot/libs/css'));
+});
