08c6e14c4d6217959893dd278897e70f2096feef
derekscode <derekscode@github.com>
2016-04-12 15:33:34 -0700

    Finally everything works right

--- src/Angular2AspNetCoreSeed/wwwroot/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../gulpfile.js" />
-/// <reference path="app/app.component.js" />
-/// <reference path="app/main.js" />
-/// <reference path="js/site.js" />
-/// <reference path="libs/bootstrap.js" />
